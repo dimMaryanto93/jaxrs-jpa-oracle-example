1da83906511782f8180e83210410f86c33d695c7
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-20 21:55:46 +0700

    update set password

--- setPassword.sh
+++ setPassword.sh
@@ -5,6 +5,7 @@ ORACLE_PWD=$1
 su -p oracle -c "sqlplus / as sysdba << EOF
       ALTER USER SYS IDENTIFIED BY "$ORACLE_PWD";
       ALTER USER SYSTEM IDENTIFIED BY "$ORACLE_PWD";
+      ALTER USER HR IDENTIFIED BY hr ACCOUNT UNLOCK;
       exit;
 EOF"
 
