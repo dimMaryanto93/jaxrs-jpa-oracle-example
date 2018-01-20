d36f3f706211751fd1c422b6e57abe32c85b2636
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-20 12:29:53 +0700

    Internal Exception: java.sql.SQLException: No suitable driver found for jdbc:oracle:thin:@localhost:1521:XE

--- src/main/java/com/maryanto/dimas/example/api/NasabahController.java
+++ src/main/java/com/maryanto/dimas/example/api/NasabahController.java
@@ -1,18 +1,43 @@
 package com.maryanto.dimas.example.api;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.maryanto.dimas.example.entity.Nasabah;
+
 @Path("/nasabah")
 public class NasabahController {
-	
+
+	// @PersistenceContext(unitName = "persistenceUnit", type =
+	// PersistenceContextType.TRANSACTION)
+	EntityManager entityManager;
+
+	public NasabahController() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistenceUnit");
+		this.entityManager = factory.createEntityManager();
+	}
+
 	@GET
 	@Path("/value")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getNasabah() {
 		return "{ \"nama\" : \"Dimas Maryanto\" }";
+	}
+
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Nasabah> findAll() {
+		Query query = entityManager.createNamedQuery("from * from Nasabah");
+		return query.getResultList();
 	}
 
 }
---
+++ src/main/java/com/maryanto/dimas/example/entity/Nasabah.java
@@ -0,0 +1,17 @@
+package com.maryanto.dimas.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MASTER_NASABAH")
+public class Nasabah {
+
+	@Id
+	@Column(name = "customer_information", nullable = false, unique = true)
+	private String cif;
+	@Column(name = "nama_lengkap", nullable = false, length = 50)
+	private String namaLengkap;
+}
