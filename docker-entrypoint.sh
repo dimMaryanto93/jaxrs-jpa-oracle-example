21b8d3cb0967acac9c976ce8e072786cb9adabc0
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-21 00:31:26 +0700

    update entrypoint docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+until 
+    su -p oracle -c "sqlplus / as sysdba << EOF
+        ALTER USER SYS IDENTIFIED BY admin;
+        ALTER USER SYSTEM IDENTIFIED BY admin;
+        ALTER USER HR IDENTIFIED BY HR ACCOUNT UNLOCK;
+        exit;
+      EOF"; do 
+      >&2 echo "Oracle database sleeping..."
+      sleep 1
+done
+
+echo "User granted!"
