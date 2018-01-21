6758b32c63d9a4f3ce3b3112d2840751663cc472
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-01-21 15:34:31 +0700

    error fixing

--- src/main/java/com/maryanto/dimas/example/api/NasabahController.java
+++ src/main/java/com/maryanto/dimas/example/api/NasabahController.java
@@ -1,3 +1,4 @@
+
 package com.maryanto.dimas.example.api;
 
 import java.util.List;
@@ -8,7 +9,9 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.maryanto.dimas.example.entity.Nasabah;
@@ -24,18 +27,65 @@ public class NasabahController {
 	}
 
 	@GET
-	@Path("/value")
+	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getNasabah() {
-		return "{ \"nama\" : \"Dimas Maryanto\" }";
+	public Nasabah getNasabah(@PathParam("id") Integer id) {
+		return this.entityManager.find(Nasabah.class, id);
+	}
+
+	@GET
+	@Path("/listByNama")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Nasabah> findNasabah(@QueryParam("nama") String nama) {
+
+		Query query = this.entityManager.createQuery("select n from Nasabah n where n.namaLengkap = :namaLengkap");
+		query.setParameter("namaLengkap", nama);
+		return query.getResultList();
 	}
 
 	@GET
 	@Path("/list")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Nasabah> findAll() {
-		Query query =  entityManager.createQuery("select n from Nasabah as n");
+
+		Query query = entityManager.createQuery("select n from Nasabah as n");
 		return query.getResultList();
 	}
+
+	// @Post
+	// @Path("/save")
+	// @Produces(MediaType.APPLICATION_JSON)
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// public Nasabah save(Nasabah nasabah) {
+	//
+	// entityManager.getTransaction().begin();
+	// entityManager.persist(nasabah);
+	// entityManager.getTransaction().commit();
+	// return nasabah;
+	// }
+	//
+	// @PUT
+	// @Path("/update")
+	// @Produces(MediaType.APPLICATION_JSON)
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// public Nasabah update(Nasabah nasabah) {
+	//
+	// this.entityManager.getTransaction().begin();
+	// nasabah = this.entityManager.merge(nasabah);
+	// this.entityManager.getTransaction().commit();
+	// return nasabah;
+	//
+	// }
+	//
+	// @DELETE
+	// @Path("/remove")
+	// @Produces(MediaType.TEXT_PLAIN)
+	// public String remove(@QueryParam("id") Integer id) {
+	//
+	// this.entityManager.getTransaction().begin();
+	// this.entityManager.remove(this.getNasabah(id));
+	// this.entityManager.getTransaction().commit();
+	// return "Berhasil dihapus";
+	// }
 
 }
